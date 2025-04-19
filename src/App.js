0c2fc065575aac5214502cc4ee8f10279d0476cb
Agnieszka Łupińska <agnieszka.gesiewska@o2.pl>
2025-04-19 05:21:56 +0200

    page Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Post from "./components/pages/Post";
+import AddPost from "./components/pages/AddPost";
+import NotFound from "./components/pages/NotFound";
+import EditPost from "./components/pages/EditPost";
+import { Container } from "react-bootstrap";
+import Header from "./components/views/Header";
+import Footer from "./components/views/Footer";
 
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Container className="container">
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/post/:id" element={<Post/>} />
+        <Route path="/post/add" element={<AddPost/>} />
+        <Route path="/post/edit/:id" element={<EditPost />}/>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Container>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { getAllPost } from "../../redux/postsRedux";
+import { useSelector } from "react-redux";
+import { Card, Col, Container, Row, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    const posts = useSelector(state => getAllPost(state));
+
+    return (
+        <>
+            <Stack direction="horizontal" className="mb-4">
+                <h1 className="me-auto">All posts</h1>
+            </Stack>
+            <Container>
+                <Row>
+                    {posts.map(post =>(
+                    <Col md="4" key={post.id} className="mb-4">
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <Card.Title>{post.title}</Card.Title>
+                                <p className="mb-2">
+                                    <strong>Author: </strong>{post.author}
+                                    <br/>
+                                    <strong>Published: </strong>{post.publishedDate}
+                                </p>
+                                <Card.Text>{post.shortDescription}</Card.Text>
+                                <Link to={`/post/${post.id}`} className="btn btn-primary">Read more</Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    ))}
+                </Row>
+            </Container>
+        </>
+    )
+}
+
+export default Home;
